eed5a7e92aedd3107e0880a1c41163e625d26c61
JocelyneGonzalezHernandez <jocelynegh1@gmail.com>
2024-09-25 18:18:06 -0600

    permiso gps
    se habilitó el permiso de gps

--- app/src/main/java/com/icm/igosafeapp/SignInPhone.kt
+++ app/src/main/java/com/icm/igosafeapp/SignInPhone.kt
@@ -54,7 +54,7 @@ class SignInPhone : AppCompatActivity() {
 
     private fun rutaVehiculo() {   //PRUEBA
         btnSend2.setOnClickListener {
-            val intent = Intent(this, ruta_vehicular::class.java)
+            val intent = Intent(this, modalidad::class.java)
             startActivity(intent)
         }
     }
